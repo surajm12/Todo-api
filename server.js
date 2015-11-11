c44aaa1c39e8b55c9905d7a9b24b4038fc7d9322
Suraj Mistry <abcvolvo12@gmail.com>
2015-11-12 01:34:32 +0530

    Add DELETE todos route

--- server.js
+++ server.js
@@ -29,9 +29,6 @@ app.get('/todos/:id', function (req, res) {
     }
 });
 
-app.listen(PORT, function () {
-  console.log('Express listening at port ' + PORT + '!');
-});
 
 //POST /todos
 app.post('/todos', function (req, res) {
@@ -47,4 +44,21 @@ app.post('/todos', function (req, res) {
     
 //    console.log('description: ' + body.description);
         res.json(body);        
+});
+
+//DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+   var todoId = parseInt(req.params.id, 10);
+   var matchedTodo = _.findWhere(todos, {id: todoId});
+    
+    if(!matchedTodo) {
+      res.status(404).json({"error": "no todo found with that id"});
+    }else {
+      todos = _.without(todos, matchedTodo);
+      res.json(matchedTodo);
+    }
+});
+
+app.listen(PORT, function () {
+  console.log('Express listening at port ' + PORT + '!');
 });
