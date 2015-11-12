6ab2d29c87c678850654816c8ab1c653b7a97cf8
Suraj Mistry <abcvolvo12@gmail.com>
2015-11-12 10:06:41 +0530

    Final static todos

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-// GET /todos?completed=true
+// GET /todos?completed=true&q=work
 app.get('/todos', function (req, res) {
     var queryParams = req.query;
     var filteredTodos = todos;
@@ -23,6 +23,13 @@ app.get('/todos', function (req, res) {
     } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
       filteredTodos = _.where(filteredTodos, {completed: false});
     }
+    
+    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+       filteredTodos = _.filter(filteredTodos, function(todo){
+          return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+       });
+    }
+        
 	res.json(filteredTodos);
 });
 
